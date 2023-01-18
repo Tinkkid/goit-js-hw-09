8d7b6913dd19c15bb96d4c92268ef2c2b1845149
Yuliia Kostovynska <y.kostovynska@gmail.com>
2023-01-18 21:49:28 +0100

    add setInterval to first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,28 +1,37 @@
 
-// Оголошуємо змінні для доступу до кнопок та тіла документа
-const startBtn = document.querySelector('button[data-start]');
-const stopBtn = document.querySelector('button[data-stop]');
-const body = document.querySelector('body');
-const timeOutId = null;
-
-
-  // Додаємо слухачі на кнопки старт та стоп
-  startBtn.addEventListener('click', onClickStartBtn);
-stopBtn.addEventListener('click', onClickStoptBtn);
-
-// Виконання функції при натисканні кнопок старт та стоп
-function onClickStartBtn() { 
-   const randomColor = getRandomHexColor();
-   const timeOutId = setTimeout(() => {
-   body.style.backgroundColor = randomColor;
-}, 1000);
-};
-
-function onClickStoptBtn() {
-};
-
-
 // Функція для генерування випадкового кольору фону
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// Оголошуємо змінні для доступу до кнопок та тіла документа
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timeId = null;
+// const randomColor = getRandomHexColor();
+
+
+  // Додаємо слухачі на кнопки старт та стоп
+startBtn.addEventListener('click', changeBgColor);
+stopBtn.addEventListener('click', onClickStoptBtn);
+
+// Виконання функції при натисканні кнопок старт та стоп
+// function changeColor() {
+//   body.style.backgroundColor = getRandomHexColor();
+// }
+
+// Створюємо функцію для зміни кольору фона 
+function changeBgColor() {
+  // changeColor();
+  timeId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+// const timeId = setInterval(changeBgColor, 1000);
+
+function onClickStoptBtn() {
+  clearInterval(timeId);
+};
+
