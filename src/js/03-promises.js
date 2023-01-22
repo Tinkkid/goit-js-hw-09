c50419280d69f42956d9659f8711241c858753ca
Yuliia Kostovynska <y.kostovynska@gmail.com>
2023-01-22 14:39:53 +0100

    done third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,9 +1,40 @@
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.6.min.css';
+
+const form = document.querySelector('.form');
+const createPromiseBtn = form.querySelector('button');
+
+createPromiseBtn.addEventListener('click', onClickCreateBtn);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve ({ position, delay })
+      } else {
+        reject ({ position, delay })
+      }
+    }, delay)
+  });  
+}
+
+function onClickCreateBtn() {
+  let delay = form.elements.delay.value;
+  const step = form.elements.step.value;
+  const amount = form.elements.amount.value;
+  console.log(delay);
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+    delay += step;
   }
 }
-const x = 5;
+
+
+
